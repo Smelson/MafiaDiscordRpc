a8c73572f97b77696ccf9d071a1eb6e5b80842f9
Shatilov Ivan <boss.vanek12@mail.ru>
2023-02-02 11:30:43 +0300

    Little fix in Log

---
+++ MafiaRpc/src/Log.cpp
@@ -0,0 +1,8 @@
+void Log(const char* message)
+{
+#ifdef DEBUG
+
+	printf("[INFO] - %s\n", message);
+
+#endif
+}
--- MafiaRpc/src/Log.hpp
+++ MafiaRpc/src/Log.hpp
@@ -1,9 +1,2 @@
 #pragma once
-void Log(const char* message)
-{
-#ifdef DEBUG
-
-	printf("[INFO] - %s\n", message);
-
-#endif
-}
+void Log(const char* message);
